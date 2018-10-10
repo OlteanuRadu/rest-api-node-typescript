f9088132bd936375ceb2d3215054ea09fa009eca
Olteanu Radu <radu.olteanu@vizolution.co.uk>
2018-10-10 11:39:12 +0300

    first commit

---
+++ lib/app.ts
@@ -0,0 +1,34 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { Routes } from "./routes/crmRoutes";
+import * as mongoose from "mongoose";
+
+class App {
+
+    public app: express.Application;
+    public routePrv: Routes = new Routes();
+    // public mongoUrl: string = 'mongodb://localhost/CRMdb';  
+    public mongoUrl: string = "mongodb://vmadmin:CNhqRKztEdGPtvVNwxrtYfKx7slVbtHpC1BuIhJNfjm9LXyl0AW8C9holQhKN9lq9hPVbQMX1lBezWjS6Trptw==@vmadmin.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+
+    constructor() {
+        this.app = express();
+        this.config();        
+        this.routePrv.routes(this.app);     
+        this.mongoSetup();
+    }
+
+    private config(): void{
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+        // serving static files 
+        this.app.use(express.static('public'));
+    }
+
+    private mongoSetup(): void{
+        mongoose.Promise = global.Promise;
+        mongoose.connect("mongodb://vmadmin:CNhqRKztEdGPtvVNwxrtYfKx7slVbtHpC1BuIhJNfjm9LXyl0AW8C9holQhKN9lq9hPVbQMX1lBezWjS6Trptw%3D%3D@vmadmin.documents.azure.com:10255/?ssl=true");        
+    }
+
+}
+
+export default new App().app;
